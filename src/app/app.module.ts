eec3d40b494e4fc805f9b6a912d5631d7b0b7c13
priscillapepe <priscillaungai99@gmail.com>
2020-08-30 12:54:37 +0300

    deleted 2 folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubSearchComponent
   ],
   imports: [
     BrowserModule,
